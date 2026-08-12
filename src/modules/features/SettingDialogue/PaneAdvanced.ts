12a3958acc617181a0e2a8c4c1eed06b17f82fdf
supravietuitor <103352537+supravietuitor@users.noreply.github.com>
2026-08-12 22:15:02 +0800

    fix: 使用动态翻译键处理远端面板

--- src/modules/features/SettingDialogue/PaneAdvanced.ts
+++ src/modules/features/SettingDialogue/PaneAdvanced.ts
@@ -2,7 +2,7 @@ import { ChunkAlgorithmNames } from "@vrtmrz/livesync-commonlib/compat/common/ty
 import { LiveSyncSetting as Setting } from "./LiveSyncSetting.ts";
 import type { ObsidianLiveSyncSettingTab } from "./ObsidianLiveSyncSettingTab.ts";
 import type { PageFunctions } from "./SettingPane.ts";
-import { $msg } from "@/common/translation";
+import { $msg, $t } from "@/common/translation";
 
 export function paneAdvanced(this: ObsidianLiveSyncSettingTab, paneEl: HTMLElement, { addPanel }: PageFunctions): void {
     void addPanel(paneEl, $msg("Memory cache")).then((paneEl) => {
@@ -48,7 +48,7 @@ export function paneAdvanced(this: ObsidianLiveSyncSettingTab, paneEl: HTMLEleme
         //         clampMax: 100, clampMin: 1, onUpdate: onlyOnCouchDB
         //     })
     });
-    void addPanel(paneEl, $msg("Remote Database Tweak")).then((paneEl) => {
+    void addPanel(paneEl, $t("Remote Database Tweak")).then((paneEl) => {
         new Setting(paneEl).setClass("wizardHidden").autoWireToggle("enableCompression");
     });
 }
